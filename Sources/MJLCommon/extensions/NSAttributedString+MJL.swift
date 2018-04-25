09dedd3bbd8831335aa9333a7ce4d4d72ef84158
Mark Lilback <mark@lilback.com>
2018-04-25 07:59:57 -0400

    added methods to replace contents of NSMutableAttributedString

---
+++ Sources/MJLCommon/extensions/NSAttributedString+MJL.swift
@@ -0,0 +1,25 @@
+//
+//  NSAttributedString+MJL.swift
+//
+//  Copyright ©2018 Mark Lilback. This file is licensed under the ISC license.
+//
+
+import Foundation
+
+public extension NSMutableAttributedString {
+	/// Replaces this string with contents
+	///
+	/// - Parameter contents: An attributed string to set this string's content to
+	public func replace(with contents: NSAttributedString) {
+		deleteCharacters(in: NSRange(location: 0, length: length))
+		setAttributedString(contents)
+	}
+
+	/// Replaces this string with contents
+	///
+	/// - Parameter contents: A string to set this string's content to (discarding all attributes)
+	public func replace(with contents: String) {
+		deleteCharacters(in: NSRange(location: 0, length: length))
+		setAttributedString(NSAttributedString(string: contents))
+	}
+}
