ee96f8f150d41e7ee874dc989133fc869685f612
Mark Lilback <mark@lilback.com>
2018-05-01 16:35:38 -0400

    confirm alert immediately calls handler if suppression key is true

--- Sources/MJLCommon/extensions/NSViewController+Alerts.swift
+++ Sources/MJLCommon/extensions/NSViewController+Alerts.swift
@@ -9,18 +9,22 @@ import Cocoa
 import SwiftyUserDefaults
 
 public extension NSViewController {
-	/// Displays an alert sheet to confirm an action
+	/// Displays an alert sheet to confirm an action. If the defaults value for the suppressionKey is true, the handler is immediately called with true.
 	///
 	/// - Parameters:
 	///   - message: The message to display
 	///   - infoText: The informative text to display
 	///   - buttonTitle: The title for the rightmost/action button
 	///   - defaultToCancel: true if the cancel button should be the default button. defaults to false
-	///   - suppressionKey: Optional UserDefaults key to set to true if user selects to suppress future alerts. If nil, suppression checkbox is not displayed. Defaults to nil
+	///   - suppressionKey: Optional UserDefaults key to set to true if user selects to suppress future alerts. If nil, suppression checkbox is not displayed. Defaults to nil. If the key value is true, no sheet is shown and the handler is immediately called with a value of true.
 	///   - handler: called after the user has selected an action
 	///   - confirmed: true if the user selected the action button
 	func confirmAction(message: String, infoText: String, buttonTitle: String, cancelTitle: String = NSLocalizedString("Cancel", comment: ""), defaultToCancel: Bool = false, suppressionKey: String? = nil, handler: @escaping (_ confirmed: Bool) -> Void)
 	{
+		if let key = suppressionKey, UserDefaults.standard.bool(forKey: key) {
+			handler(true)
+			return
+		}
 		precondition(view.window != nil,"can't call without being in a window")
 		let alert = NSAlert()
 		alert.showsSuppressionButton = suppressionKey != nil
@@ -40,7 +44,7 @@ public extension NSViewController {
 		})
 	}
 
-	/// Displays an alert sheet to confirm an action. This versions takes a supression key in the format used by SwiftyUserDefaults. If such a value is not supplied or is a string, another version of this function is called.
+	/// Displays an alert sheet to confirm an action. This versions takes a supression key in the format used by SwiftyUserDefaults. If such a value is not supplied or is a string, another version of this function is called. If the defaults value for the suppressionKey is true, the handler is immediately called with true.
 	///
 	/// - Parameters:
 	///   - message: The message to display
@@ -52,6 +56,10 @@ public extension NSViewController {
 	///   - confirmed: true if the user selected the action button
 	func confirmAction(message: String, infoText: String, buttonTitle: String, cancelTitle: String = NSLocalizedString("Cancel", comment: ""), defaultToCancel: Bool = false, suppressionKey: DefaultsKey<Bool>, handler: @escaping (_ confirmed: Bool) -> Void)
 	{
+		guard !UserDefaults.standard[suppressionKey] else {
+			handler(true)
+			return
+		}
 		let alert = NSAlert()
 		alert.showsSuppressionButton = true
 		alert.messageText = message
